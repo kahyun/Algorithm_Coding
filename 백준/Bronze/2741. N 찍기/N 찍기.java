24a75ffbf256c899733099d45b909630d70fc8d0
kahyun <ahdalahdahd@gmail.com>
2025-04-30 19:23:40 +0900

    [Bronze V] Title: N 찍기, Time: 796 ms, Memory: 32476 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		for(int i=1; i<=m;i++) {
+			System.out.println(i);
+		}
+	}
+}
