ed7099d4fce5b1187e4cb6b5666b279d29ebc590
kahyun <ahdalahdahd@gmail.com>
2025-05-02 08:34:16 +0900

    [Bronze V] Title: A+B - 3, Time: 196 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -7,11 +7,14 @@ public class Main {
 		int t = sc.nextInt();
 		int A = 0;
 		int B = 0;
+		int[] result = new int[t];
 		for(int i = 0; i<t; i++) {
 			A = sc.nextInt();
 			B = sc.nextInt();
-			int result = A+B;
-			System.out.println(result);
+			result [i] = A+B;
+		}
+		for(int i = 0; i<t; i ++) {
+		System.out.println(result[i]);
 		}
 	}
 }
